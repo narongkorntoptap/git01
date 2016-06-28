ff34eab2c8c6a1a4de94f0cfe82d89fbc8e066b3
jirasin <mark.i3gateway@gmail.com>
2016-06-28 14:02:30 +0700

    no message

--- application/views/input_diary/input.php
+++ application/views/input_diary/input.php
@@ -15,14 +15,14 @@
 			  	<div class="form-group">
 			    	<label for="inputPassword3" class="col-sm-2 control-label">รายรับ</label>
 			    	<div class="col-sm-10">
-			      		<input type='text' name='username' class="form-control inputform" value="<?php echo set_value("username"); ?>">
+			      		<input type='text' name='inmoney' class="form-control inputform" value="<?php echo set_value("inmoney"); ?>">
 			   		</div>
 			  	</div>
 
 			  	<div class="form-group">
 			    	<label for="inputPassword3" class="col-sm-2 control-label">รายจ่าย</label>
 			    	<div class="col-sm-10">
-			      		<input type='text' name='first_name' class="form-control inputform" value="<?php echo set_value("first_name"); ?>">
+			      		<input type='text' name='outmoney' class="form-control inputform" value="<?php echo set_value("outmoney"); ?>">
 			   		</div>
 			  	</div>
 
