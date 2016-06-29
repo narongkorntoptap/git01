0cc7122b3d8a6e3b3b2b31bf356fd6125dd5dc44
noontanatip <tanatip_kuku@hotmail.com>
2016-06-29 09:07:43 +0700

    no message

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,7 @@
   		<section id="footer">
   			<div class="container-fluid iBannerFix ">
   				<div class="col-md-12 ">
-  					<h4 align="center" class="footer__text">TOP</h4>
+  					<h4 align="center" class="footer__text">~ TOP ~ MARK ~ MAY ~ NOON ~</h4>
   				</div>
   			</div>
   		</section>
