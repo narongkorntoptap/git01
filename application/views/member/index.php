2c882961eff1610f220c710be3121ef2bf49b64a
jirasin <mark.i3gateway@gmail.com>
2016-07-01 14:35:37 +0700

    no message

--- application/views/member/index.php
+++ application/views/member/index.php
@@ -1,7 +1,22 @@
 
 <section id="index">
 <?php $this->load->view('template/header'); ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>
+	$(document).ready(function(){
+   		$("#flip").click(function(){
+        	$("#panel").slideToggle("slow");
+    	});
+	});
+</script>
 
+<script>
+	$(document).ready(function(){
+   		$("#flip2").click(function(){
+        	$("#panel2").slideToggle("slow");
+    	});
+	});
+</script>
 
 		<table align="center" >
 			<tr>
@@ -12,9 +27,10 @@
 					<button id="register" class="buttonuser btn-block" ><a href="<?php echo site_url('member/login'); ?>">Login</a></button>
 					
 					<button id="register" class="buttonuser btn-block"><a href="<?php echo site_url('member/create'); ?>">Register</a></button> -->
-
-					<button class="buttonuser btn-block"><a href="<?php echo site_url('member/login'); ?>">Login</a></button>
-					<button class="buttonuser btn-block"><a href="<?php echo site_url('member/create'); ?>">Register</a></button>
+					<div id="flip">click</div>
+					<button id="panel" class="buttonuser btn-block"><a href="<?php echo site_url('member/login'); ?>">Login</a></button>
+					<div id="flip2">click</div>
+					<button id="panel2" class="buttonuser btn-block"><a href="<?php echo site_url('member/create'); ?>">Register</a></button>
 
 				</td>	
 			</tr>
