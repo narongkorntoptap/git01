22434c32faf8f650cad9205fb12ccf2fd5cd9ba2
Maykusrc <maykusrc@gmail.com>
2016-07-01 14:07:20 +0700

    no message

--- application/views/member/index.php
+++ application/views/member/index.php
@@ -1,30 +1,15 @@
+
 <section id="index">
 <?php $this->load->view('template/header'); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script>
-		$(document).ready(function(){
-		    $("#flip").click(function(){
-		        $("#login").slideToggle("slow");
-		    });
-		});
-	</script>
-	<script>
-		$(document).ready(function(){
-		    $("#flip2").click(function(){
-		        $("#register").slideToggle("slow");
-		    });
-		});
-	</script>
+
  
 
-		<table align="center" width="60%" height="100%">
+		<table align="center" >
 			<tr>
 				<td>
-					<div id="flip">click</div>
-					<!-- <button id="flip" class="buttonuser btn-block">click login</button> -->
-					<button id="login" class="buttonuser btn-block"><a href="<?php echo site_url('member/login'); ?>">Login</a></button>
-					<div id="flip2">click</div>
-					<!-- <button id="flip2" class="buttonuser btn-block">click register</button> -->
+					
+					<button id="register" class="buttonuser btn-block" ><a href="<?php echo site_url('member/login'); ?>">Login</a></button>
+					
 					<button id="register" class="buttonuser btn-block"><a href="<?php echo site_url('member/create'); ?>">Register</a></button>
 				</td>	
 			</tr>
